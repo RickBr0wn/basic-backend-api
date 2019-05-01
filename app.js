817bec556780cca63987f616f59abdf55c6049a2
Rick Brown <ricky.brown.00@gmail.com>
2019-05-01 20:11:53 +0100

    Clean up & comment add

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const morgan = require('morgan')
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+// Allows request data in server console.log
 app.use(morgan('dev'))
 
+app.use((req, res, next) => {
+  // Second param in .header() limits connection
+  // * allows any connection
+  // Could be 'https//:mySite.com'
+  res.header('Access-Control-Allow-Origin', '*')
+})
+
+// Routes
 app.use('/products', productRoutes)
 app.use('/orders/', orderRoutes)
 
-// Display a 404 page
+// Display a 404 - not found page
 app.use((req, res, next) => {
   const err = new Error('404 - Not found')
   err.status = 404
   next(err)
 })
 
+// Final catch all error
 app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.json({
--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const http = require('http')
 const app = require('./app')
 
-// Date.now().toISOString()
-// process.env.MONGO_DB
-
-// using the JSON body parser
-// app.use(require('body-parser').urlencoded({ extended: false }))
-// app.use(require('body-parser').json())
-
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
 
