c04c9f148f0d2feba9028f7b6beee3cd0f15bd2e
Rick Brown <ricky.brown.00@gmail.com>
2019-05-05 15:20:08 +0100

    Added image GET & POST requests

--- API/Routes/products.js
+++ API/Routes/products.js
@@ -1,7 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const multer = require('multer')
 const Product = require('../Models/product')
+// Helper function
+const oneMegabyte = 1024 * 1024
+
+// START -> multer configuration
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, './uploads')
+  },
+  filename: function(req, file, callback) {
+    callback(null, new Date().toISOString() + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, callback) => {
+  // reject a fille
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    callback(null, true)
+  } else {
+    callback(null, false)
+  }
+}
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: oneMegabyte * 5
+  },
+  fileFilter
+})
+// END - multer configuration
 
 // @route   GET /products/
 // @desc    Get all products
@@ -9,7 +39,7 @@ const Product = require('../Models/product')
 // @body    null
 router.get('/', (req, res, next) => {
   Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
       const response = {
@@ -17,6 +47,7 @@ router.get('/', (req, res, next) => {
         products: docs.map(doc => ({
           name: doc.name,
           price: doc.price,
+          productImage: doc.productImage,
           _id: doc._id,
           request: {
             type: 'GET',
@@ -36,36 +67,43 @@ router.get('/', (req, res, next) => {
 // @desc    Post a new product
 // @access  Public
 // @body    Object - { "name": String, "price": Number }
-router.post('/', require('body-parser').json(), (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price
-  })
-  product
-    .save()
-    .then(result => {
-      console.log('Result: ', result)
-      res.status(201).json({
-        message: 'Created product successfully',
-        createdProduct: {
-          name: result.name,
-          price: result.price,
-          _id: result._id,
-          request: {
-            type: 'GET',
-            url: 'localhost:3000/products/' + result._id
+router.post(
+  '/',
+  require('body-parser').json(),
+  upload.single('productImage'),
+  (req, res, next) => {
+    console.log(req.file)
+    const product = new Product({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      price: req.body.price,
+      productImage: req.file.path
+    })
+    product
+      .save()
+      .then(result => {
+        console.log('Result: ', result)
+        res.status(201).json({
+          message: 'Created product successfully',
+          createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+              type: 'GET',
+              url: 'localhost:3000/products/' + result._id
+            }
           }
-        }
+        })
       })
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(500).json({
-        error
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({
+          error
+        })
       })
-    })
-})
+  }
+)
 
 // @route   GET /products/:productId
 // @desc    Get an individual product based on id
@@ -74,7 +112,7 @@ router.post('/', require('body-parser').json(), (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId
   Product.findById(id)
-    .select('price _id name')
+    .select('price _id name productImage')
     .exec()
     .then(doc => {
       if (doc) {
